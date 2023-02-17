38d4646d6569f65116bded4e202defa4a1d29deb
Felipe <felipe.carvalho1703@usp.br>
2023-02-17 19:57:36 -0300

    teste

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const path = require('path')
 const app = express()
 const server = require('http').createServer(app)
 
-const io = require('socket.io')(server)
+const io = require('socket.io')(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+})
 
 
 app.use(express.static(path.join(__dirname, 'public')))
